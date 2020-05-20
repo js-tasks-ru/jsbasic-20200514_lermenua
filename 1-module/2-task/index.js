ed090109cc6386db5bddd9ed23359eb2310cdd60
admin <Lermenua@yandex.ru>
2020-05-20 22:18:28 +0300

    1-module

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (name) {
+    return name.length >= 4 && !name.includes(" ");
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,6 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+  str.toLowerCase();
+  return str.includes("1XbeT") || str.includes("xxx");
 }
