885752ba09d452367f4698ab37ade4748677a4ef
admin <Lermenua@yandex.ru>
2020-05-28 16:41:12 +0300

    task 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,11 @@
  * @returns {string}
  */
 function camelize(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return str.split("-")
+      .map((elem, index) => index === 0 ? elem : elem[0].toUpperCase() + elem.slice(1))
+      .join("");
+  }
 }
