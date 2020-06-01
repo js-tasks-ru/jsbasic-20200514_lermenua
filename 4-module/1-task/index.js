db5ad1e9fd0f5857da778122befc60fb1abcc201
admin <Lermenua@yandex.ru>
2020-06-01 15:42:37 +0300

    task 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  const friendList = document.createElement("ul");
+  document.body.append(friendList);
+  friendList.innerHTML = friends.map(({firstName, lastName}) => `<li class="item">${firstName} ${lastName}</li>`);
+  return friendList;
+
+
+
+
 }
